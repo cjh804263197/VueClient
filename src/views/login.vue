6c49e3c7f724ec1f16b6141a326d4ddb31a04a01
Mr_chenjiahui@163.com <0chenjiahui0>
2018-05-17 20:53:15 +0800

    init

---
+++ src/views/login.vue
@@ -0,0 +1,90 @@
+<style lang="less">
+    @import './login.less';
+</style>
+
+<template>
+    <div class="login" @keydown.enter="handleSubmit">
+        <div class="login-con">
+            <Card :bordered="false">
+                <p slot="title">
+                    <Icon type="log-in"></Icon>
+                    欢迎登录
+                </p>
+                <div class="form-con">
+                    <Form ref="loginForm" :model="form" :rules="rules">
+                        <FormItem prop="username">
+                            <Input v-model="form.username" placeholder="请输入用户名">
+                                <span slot="prepend">
+                                    <Icon :size="16" type="person"></Icon>
+                                </span>
+                            </Input>
+                        </FormItem>
+                        <FormItem prop="password">
+                            <Input type="password" v-model="form.password" placeholder="请输入密码">
+                                <span slot="prepend">
+                                    <Icon :size="14" type="locked"></Icon>
+                                </span>
+                            </Input>
+                        </FormItem>
+                        <FormItem>
+                            <RadioGroup v-model="loginRole">
+                                <Radio label="监管用户"></Radio>
+                                <Radio label="企业用户"></Radio>
+                            </RadioGroup>
+                        </FormItem>
+                        <FormItem>
+                            <Button @click="handleSubmit" type="primary" long>登录</Button>
+                        </FormItem>
+                    </Form>
+                    <p class="login-tip">请选择对应的角色进行登录</p>
+                </div>
+            </Card>
+        </div>
+    </div>
+</template>
+
+<script>
+import Cookies from 'js-cookie';
+export default {
+    data () {
+        return {
+            form: {
+                username: '',
+                password: ''
+            },
+            loginRole: '监管用户',
+            rules: {
+                username: [
+                    { required: true, message: '账号不能为空', trigger: 'blur' }
+                ],
+                password: [
+                    { required: true, message: '密码不能为空', trigger: 'blur' }
+                ]
+            }
+        };
+    },
+    methods: {
+        handleSubmit () {
+            this.$refs.loginForm.validate((valid) => {
+                if (valid) {
+                    Cookies.set('user', this.form.userName);
+                    Cookies.set('password', this.form.password);
+                    this.$store.commit('setAvator', 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3448484253,3685836170&fm=27&gp=0.jpg');
+                    if (this.form.userName === 'iview_admin') {
+                        Cookies.set('access', 0);
+                    } else {
+                        Cookies.set('access', 1);
+                    }
+                    this.$router.push({
+                        name: 'home_index'
+                    });
+                }
+            });
+        }
+    }
+};
+</script>
+
+<style>
+
+</style>
